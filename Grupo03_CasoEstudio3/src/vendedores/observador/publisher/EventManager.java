55399a48b26202acbf6ee97876a06b5260f00011
asozas <asozas@ucenfotec.ac.cr>
2023-07-23 09:31:44 -0600

    Implementacion del EventManager

--- Grupo03_CasoEstudio3/src/vendedores/observador/publisher/EventManager.java
+++ Grupo03_CasoEstudio3/src/vendedores/observador/publisher/EventManager.java
@@ -1,8 +1,13 @@
 package vendedores.observador.publisher;
 
+
 import vendedores.observador.listener.IListener;
 
-import java.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class EventManager {
     Map<String, List<IListener>> listeners = new HashMap<>();
@@ -10,6 +15,22 @@ public class EventManager {
     public EventManager(String... operations){
         for (String operation : operations) {
             this.listeners.put(operation, new ArrayList<>());
+        }
+    }
+    public void subscribe(String eventType, IListener listener) {
+        List<IListener> users = listeners.get(eventType);
+        users.add(listener);
+    }
+
+    public void unsubscribe(String eventType, IListener listener) {
+        List<IListener> users = listeners.get(eventType);
+        users.remove(listener);
+    }
+
+    public void notify(String eventType) {
+        List<IListener> users = listeners.get(eventType);
+        for (IListener listener : users) {
+            listener.update(eventType);
         }
     }
 

b295da057f6bcb8a88f4fe8454c7256cd3fc639a
Joseph Murillo <josephmurillo2425@gmail.com>
2023-07-26 18:48:31 -0600

    add proxy

--- Grupo03_CasoEstudio3/src/bl/Proxy/UsuarioProxy.java
+++ Grupo03_CasoEstudio3/src/bl/Proxy/UsuarioProxy.java
@@ -1,4 +1,26 @@
 package bl.Proxy;
 
-public class UsuarioProxy {
+import bl.entities.builder.objects.Usuario;
+
+public class UsuarioProxy extends Usuario {
+    private Usuario usuario;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public boolean analizarRol() {
+        String rol = usuario.getRol();
+        if(rol == "Vendedor") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
